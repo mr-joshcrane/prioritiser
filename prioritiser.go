5590f90bae4e51d81328a82173a51fa8778ab42a
Josh Crane <josh.crane@mechanicalrock.io>
2022-05-23 14:57:07 +0800

    Adding test for previously sorted input

--- prioritiser.go
+++ prioritiser.go
@@ -3,8 +3,9 @@ package prioritiser
 import (
 	"fmt"
 	"io"
-	"os"
+	"math/rand"
 	"sort"
+	"time"
 )
 
 func GetUserPreference[T any](a, b T, r io.Reader, w io.Writer) T {
@@ -24,8 +25,15 @@ func GetUserPreference[T any](a, b T, r io.Reader, w io.Writer) T {
 
 func RunCLI(items []string, r io.Reader, w io.Writer) {
 	sort.Slice(items, func(i, j int) bool {
-		pref:= GetUserPreference(items[i], items[j], os.Stdin, os.Stdout)
-		return pref == items[i] 
+		pref := GetUserPreference(items[i], items[j], r, w)
+		return pref == items[i]
 	})
-	fmt.Println(items)
+	fmt.Fprintln(w, items)
+}
+
+func RandomList() []string {
+	list := []string{"1", "2", "3", "4", "5", "6", "7", "8", "9", "10"}
+	rand.Seed(time.Now().Unix())
+	rand.Shuffle(len(list), func(i, j int) { list[i], list[j] = list[j], list[i] })
+	return list
 }
--- prioritiser_test.go
+++ prioritiser_test.go
@@ -12,7 +12,7 @@ import (
 func TestGetUserPreferenceGivenAAndBReturnsAIfUserEntersY(t *testing.T) {
 	t.Parallel()
 	r := strings.NewReader("y")
-	got := prioritiser.GetUserPreference("preferred", "non-preferred" , r, io.Discard)
+	got := prioritiser.GetUserPreference("preferred", "non-preferred", r, io.Discard)
 	want := "preferred"
 	if want != got {
 		t.Fatalf("wanted %v, got %v", want, got)
@@ -20,8 +20,29 @@ func TestGetUserPreferenceGivenAAndBReturnsAIfUserEntersY(t *testing.T) {
 }
 
 func TestRunCLIReturnsItemsInDescendingOrderOfImportance(t *testing.T) {
+	t.Parallel()
 	input := []string{"high", "low", "medium"}
 	buf := &bytes.Buffer{}
-	r := strings.NewReader("y\nn\nn")
+
+	want := "Do you prefer low over high?\nDo you prefer medium over low?\nDo you prefer medium over high?\n[high medium low]\n"
+	r := strings.NewReader("n\ny\nn\n")
+	
 	prioritiser.RunCLI(input, r, buf)
+	got := buf.String()
+	if want != got {
+		t.Fatalf("wanted %v, got %v", want, got)
+	}
 }
+
+func TestCanInsertNewItemsIntoPreviouslySortedList(t *testing.T) {
+	t.Parallel()
+	prevSorted := []string{"high", "low", "medium"}
+	newItems := []string{"highest", "lowest"}
+	want := []string{"highest", "high", "medium",  "low", "lowest"}
+	got := prioritiser.Sort(prevSorted, newItems)
+	if want != got {
+		t.Fatalf("wanted %v, got %v", want, got)
+	}
+}
+
+
